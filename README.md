c426e0ed51462a007b7ff6fb8909ccffbea7ebe6
cabalex <31020729+cabalex@users.noreply.github.com>
2025-08-20 00:21:18 -0700

    Add additional privacy disclaimers
    Read my code! Inspect everything! Be safe out there!

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
 # [GET Tools](https://cabalex.github.io/get-tools)
+
 Tools for interacting with CBORD's GET app, featuring:
+
 - Detailed spending analytics
 - QR code generation
-- Custom deposit amounts *(saved cards only)*
+- Custom deposit amounts _(saved cards only)_
 - Code sharing
 
 This app is not affiliated with CBORD or GET. It's just a passion project for fun!
 
 ## Security
-Since CBORD's API is exposed with CORS headers, the app works by directly sending requests to the GET API. No information is ever sent to a third party server. GET Tools is a static site, and doesn't receive any of your data!
 
-## What gets shared when I share a code?
+**You should know that giving account access to any external program comes with risks.** A service like this could easily run off with your points, and if it's hosted outside of a site like `github.io` (where you can directly see the source code), you can't be sure that it won't. _Never, ever_ give services access to sensitive information without knowing what they will do, and how they will handle your data.
+
+GET Tools hooks into CBORD's API to connect with the GET app. This API is exposed with Cross-Origin Resource Sharing (CORS) headers - for non-webdev people, that means your device can access the API through my website _without sending data to an external server._
+
+GET Tools is compiled into a static site here on GitHub - it's just HTML, JS, and CSS being served on the internet. There's no secret backend server, no databases, no account system, and there never will be. Your API key and everything personal is stored on your device, and you can delete that data at any time by clearing the site's data in your browser.
+
+Still hesitant? Good! I encourage you to Inspect Element and check the Network tab to see what's actually being sent back and forth. You can also poke around in [my code](https://github.com/cabalex/get-tools/blob/main/src/getStore.ts) and see what it does. That's the magic of open source!
+
+I created this app for my own benefit, wanting more out of the official app that CBORD provides. I'm sharing it for my own convenience (I like hosting on GitHub 😀), and you're welcome to use it too, but please note **this app is provided as-is, and I do not take any responsibility for any action caused by the usage of this app, whether by you or by other users.**
+
+## FAQ
+
+### Can GET Tools see my credit cards?
+
+GET Tools will never be able to see your full payment information. All that it can do is:
+
+- See the cardholder's name and last four digits
+- Deposit money into one of your accounts using a saved payment method (in-app confirmation required)
+- Remove payment methods from your account
+
+None of this data is saved. GET Tools fetches this data every time you reload the page.
+
+If this sounds scary, I recommend you remove **all** payment methods from your GET account before linking. And **if you're sharing a code, you should always remove them!** (There's even a handy button to do so!)
+
+### What gets shared when I share a code?
+
 When sharing codes, a new "device" is created with a specific ID and PIN, which is able to be revoked at any time. **Treat this like other people logging into your GET account.**
 
-Since there isn't any way to only partially access an account, it is possible to get full account access through these codes, so only share them with people you trust!
+Since there isn't any way to only partially access an account, it is possible to get full account access through these codes, so _only share them with people you really trust_! I am **not** responsible for other people misusing the codes you share.
+
+### Can GET Tools transfer balances between accounts?
+
+No. The API explicitly blocks this.
--- src/onboarding/Onboarding.svelte
+++ src/onboarding/Onboarding.svelte
@@ -41,6 +41,8 @@
             }
         }
     }
+
+    let step = 0;
 </script>
 
 <div class="onboarding">
@@ -48,19 +50,34 @@
         <p><b>GET</b>ting your data...</p>
         <Loading />
     {:else}
-        <h2>Welcome to GET Tools!</h2>
-        <p>To get started, we'll need access to your GET account.</p>
-        <p>Open this link and sign in with your UCSC account.</p>
-        <a class="btn" href="https://get.cbord.com/ucsc/full/login.php?mobileapp=1" target="_blank" rel="noopener noreferrer">
-            Log in with UCSC <IconExternalLink />
-        </a>
-        <p>Once you arrive at a page that says "activated",<br /> copy the page link and paste it here.</p>
-        <div class="linkInput">
-            <IconLink />
-            <input type="text" bind:value={linkValue} style="width: 32ch" />
-            {#if invalidLink}
-                <span style="color: red;">Hmm... this doesn't seem like a valid link. Try again?</span>
-            {/if}
+        <h2 style="margin-bottom: 20px">Welcome to GET Tools!</h2>
+        <div class="step" class:complete={step >= 1}>
+            <span class="step-number">1</span>
+            <p>Before we begin, please read how your data is handled.</p>
+            <a class="btn" href="https://github.com/cabalex/get-tools?tab=readme-ov-file#security" target="_blank" rel="noopener noreferrer" on:click={() => step = 1}>
+                No, seriously. Read it. <IconExternalLink />
+            </a>
+            <span class="small">This is <b>sensitive data</b> that pertains to <b>your stuff</b>! You should always be curious about how it's processed.</span>
+            <span class="small">By continuing, you agree to the terms listed in the link above.</span>
+        </div>
+        <div class="step" class:complete={step >= 2} class:hidden={step < 1}>
+            <span class="step-number">2</span>
+            <p>Next, we'll need access to your GET account.</p>
+            <p>Open this link and sign in with your UCSC account.</p>
+            <a class="btn" href="https://get.cbord.com/ucsc/full/login.php?mobileapp=1" target="_blank" rel="noopener noreferrer" on:click={() => step = 2}>
+                Log in with UCSC <IconExternalLink />
+            </a>
+        </div>
+        <div class="step" class:complete={step >= 3} class:hidden={step < 2} style="border-left-color: transparent;">
+            <span class="step-number">3</span>
+            <p>Once you arrive at a page that says "validated",<br /> copy the page link and paste it here.</p>
+            <div class="linkInput">
+                <IconLink />
+                <input type="text" bind:value={linkValue} />
+                {#if invalidLink}
+                    <span style="color: red;">Hmm... this doesn't seem like a valid link. Try again?</span>
+                {/if}
+            </div>
         </div>
         <span class="small">
             Your login data is only sent to GET and remains on device.<br /><a target="_blank" rel="noopener noreferrer" href="https://github.com/cabalex/get-tools?tab=readme-ov-file#security">See how we process your data</a>
@@ -78,7 +95,6 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-        gap: 10px;
         max-width: 500px;
         margin: auto;
         text-align: center;
@@ -97,12 +113,63 @@
         background-color: #111;
         color: #eee;
         font-size: 1em;
-        width: 100%;
+        width: 32ch;
+        max-width: calc(100vw - 150px);
     }
     .small {
         color: #555;
     }
     h2, p {
         margin: 0;
+    }
+    .step {
+        width: calc(100% - 30px);
+        border-left: 2px solid #ccc;
+        padding: 10px;
+        padding-left: 30px;
+        padding-top: 0;
+        padding-bottom: 30px;
+        margin-left: 10px;
+        position: relative;
+        text-align: left;
+        
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: center;
+        gap: 10px;
+        transition: opacity 0.3s, border-color 0.3s;
+    }
+    .step.hidden {
+        opacity: 0.5;
+        pointer-events: none;
+    }
+    .step:not(.hidden):not(.complete) .step-number {
+        background-color: #222;
+        color: #eee;
+    }
+    .step.hidden .btn {
+        background-color: #ccc;
+    }
+    .step .step-number {
+        position: absolute;
+        left: -1px;
+        transform: translateX(-50%);
+        background-color: #ccc;
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+        top: 0;
+        font-weight: bold;
+        color: #555;
+        text-align: center;
+        transition: background-color 0.3s, color 0.3s;
+    }
+    .step.complete {
+        border-color: green;
+    }
+    .step.complete .step-number {
+        background-color: green;
+        color: #fff;
     }
 </style>
